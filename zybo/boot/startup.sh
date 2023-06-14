a9f273308878754c983405558cd692b36fc00e4e
jteglund <jonas.teglund.6494@student.uu.se>
2023-06-14 11:18:10 +0200

    Added a bugfix for permission denied issue on Mario

--- zybo/boot/startup.sh
+++ zybo/boot/startup.sh
@@ -3,4 +3,6 @@
 # Startup script after boot to initiate user program. Requires a working ethernet connection
 cd /home/alarm
 tftp 10.0.0.1 69 -c get program /home/alarm/program
+#Uncomment line bellow if permission denied
+#chmod a=rwx /home/alarm/program
 ./program
