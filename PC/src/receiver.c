a80da6db3d29b506b3f3a18127c7e999369d81fc
Irreq <irreq@protonmail.com>
2023-06-19 17:46:36 +0200

    Update receiver.c

--- PC/src/receiver.c
+++ PC/src/receiver.c
@@ -118,71 +118,7 @@ void test()
     // printf("Time taken: %f\n", timeElapsed);
 }
 
-/* Like convolve_avx_unrolled_vector_unaligned but using FMA
- * */
-int convolve_avx_unrolled_vector_unaligned_fma(float *in, float *out,
-                                               int length, float *kernel, int kernel_length)
-{
 
-    __m256 kernel_reverse[KERNEL_LENGTH] __attribute__((aligned(ALIGNMENT)));
-    __m256 data_block __attribute__((aligned(ALIGNMENT)));
-
-    __m256 acc0 __attribute__((aligned(ALIGNMENT)));
-    __m256 acc1 __attribute__((aligned(ALIGNMENT)));
-
-    // Repeat the kernel across the vector
-    for (int i = 0; i < KERNEL_LENGTH; i++)
-    {
-        kernel_reverse[i] = _mm256_broadcast_ss(
-            &kernel[KERNEL_LENGTH - i - 1]);
-    }
-
-    for (int i = 0; i < length - KERNEL_LENGTH; i += VECTOR_LENGTH)
-    {
-
-        acc0 = _mm256_setzero_ps();
-        acc1 = _mm256_setzero_ps();
-
-        for (int k = 0; k < KERNEL_LENGTH; k += VECTOR_LENGTH)
-        {
-
-            int data_offset = i + k;
-
-            for (int l = 0; l < SSE_SIMD_LENGTH; l++)
-            {
-
-                for (int m = 0; m < VECTOR_LENGTH; m += SSE_SIMD_LENGTH)
-                {
-
-                    data_block = _mm256_loadu_ps(
-                        in + l + data_offset + m);
-
-                    // acc0 = kernel_reverse[k+l+m] * data_block + acc0;
-                    acc0 = _mm256_fmadd_ps(
-                        kernel_reverse[k + l + m], data_block, acc0);
-
-                    data_block = _mm256_loadu_ps(in + l + data_offset + m + AVX_SIMD_LENGTH);
-
-                    // acc1 = kernel_reverse[k+l+m] * data_block + acc1;
-                    acc1 = _mm256_fmadd_ps(
-                        kernel_reverse[k + l + m], data_block, acc1);
-                }
-            }
-        }
-        _mm256_storeu_ps(out + i, acc0);
-        _mm256_storeu_ps(out + i + AVX_SIMD_LENGTH, acc1);
-    }
-
-    // Need to do the last value as a special case
-    int i = length - KERNEL_LENGTH;
-    out[i] = 0.0;
-    for (int k = 0; k < KERNEL_LENGTH; k++)
-    {
-        out[i] += in[i + k] * kernel[KERNEL_LENGTH - k - 1];
-    }
-
-    return 0;
-}
 
 int main(int argc, char const *argv[])
 {
