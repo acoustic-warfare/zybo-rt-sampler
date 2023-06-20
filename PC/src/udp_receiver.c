9a667b8ac2313be8afbd8bc4824bb74851cc83d3
jteglund <jonas.teglund.6494@student.uu.se>
2023-06-20 09:34:53 +0200

    Added defines for better generalization

--- PC/src/udp_receiver.c
+++ PC/src/udp_receiver.c
@@ -5,10 +5,18 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+#define HEADER 4
+#define N_MICROPHONES 2
+#define N_SAMPLES 32
+#define BUFFER_LENGTH HEADER + N_MICROPHONES * N_SAMPLES
+
+#define UDP_PORT 21844
+#define SERVER_IP "10.0.0.1"
+
 int main(void){
     int socket_desc;
     struct sockaddr_in server_addr, client_addr;
-    int32_t client_message[68];
+    int32_t client_message[BUFFER_LENGTH];
     int client_struct_length = sizeof(client_addr);
     
     // Clean buffers:
@@ -25,8 +33,8 @@ int main(void){
     
     // Set port and IP:
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(21844);
-    server_addr.sin_addr.s_addr = inet_addr("10.0.0.1");
+    server_addr.sin_port = htons(UDP_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
     
     // Bind to the set port and IP:
     if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
@@ -44,8 +52,7 @@ int main(void){
             printf("Couldn't receive\n");
             return -1;
         }
-
-        printf("%d \n", client_message[3]);
+        printf("%s" "%d \n", "Sample count: ", client_message[3]);
         
     }
 
