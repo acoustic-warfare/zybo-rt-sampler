a96c28dd5d8125df6669e6e5ff1a86f0d4aca269
Irreq <irreq@protonmail.com>
2023-06-20 09:44:41 +0200

    Added config file

---
+++ PC/src/config.h
@@ -0,0 +1,5 @@
+#define N_SAMPLES 256
+
+
+// For convolution
+#define N_TAPS 128
--- PC/src/main.c
+++ PC/src/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 #include "udp_receiver.h"
+#include "config.h"
 
 
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World!\n");
+    printf("Hello World! Number of samples: %d\n", N_SAMPLES);
     return 0;
 }
