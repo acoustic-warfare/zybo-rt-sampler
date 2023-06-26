81045a7dfc3cb026f723dfa70aab936e2ab67043
jteglund <jonas.teglund.6494@student.uu.se>
2023-06-26 13:48:52 +0200

    Fixed typo

---
+++ PC/realtime_samples.py
@@ -0,0 +1,36 @@
+import numpy as np
+import ctypes
+
+def get_antenna_data():
+    lib = ctypes.cdll.LoadLibrary("./lib/libsampler.so")
+
+    init = lib.load
+    init.restype = int
+
+    get_data = lib.myread
+    get_data.restype = None
+    get_data.argtypes = [
+        ctypes.POINTER(ctypes.c_float)
+    ]
+
+    init()
+    
+    return get_data
+
+
+def get_samples():
+    f = get_antenna_data()
+    samples = 256
+    N_SAMPLES = samples * 64
+
+    out = np.empty(N_SAMPLES, dtype=np.float32)
+    out_pointer = out.ctypes.data_as(
+        ctypes.POINTER(ctypes.c_float))
+    
+    while(True):
+        f(out_pointer)
+        b = out.reshape((samples, 64))
+        print(b[0])
+
+get_samples()
+exit()
