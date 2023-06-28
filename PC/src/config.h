43fb9b6eb5d28b06d8864c6b1ccf936ad8de498d
jteglund <jonas.teglund.6494@student.uu.se>
2023-06-28 11:17:07 +0200

    Tweaked config

--- PC/src/config.h
+++ PC/src/config.h
@@ -1,7 +1,7 @@
 #define N_MICROPHONES 64
 #define N_SAMPLES 128
 #define UDP_PORT 21844
-#define UDP_IP "0.0.0.0"
+#define UDP_IP "10.0.0.1"
 #define BUFFER_LENGTH N_SAMPLES * N_MICROPHONES
 #define NORM_FACTOR 262144
 #define N_TAPS 128
