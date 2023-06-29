25cbee76ec2a7c608c10e7ad064adb6b49970278
jteglund <jonas.teglund.6494@student.uu.se>
2023-06-29 16:06:39 +0200

    Comments

--- PC/camera_heatmap.py
+++ PC/camera_heatmap.py
@@ -30,14 +30,14 @@ def display_camera():
     # Setup camera
     cap = cv2.VideoCapture(0)
 
-    # Read heatmap and resize
 #    print("IMPORTANT: YOU NEED PHOTO PERMISSION IF YOU ARE AT A SECURE/VITAL INSTALLATION!!!")
  #   if input("Do you have a valid photo permission? (Y/n)") == "Y":
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
-        # Add heatmap to current frame
+        # Generate a random heatmap
         heatmap = simulate_heatmap()
+        # Add heatmap to current frame
         dst = cv2.addWeighted(frame, 0.6, heatmap, 0.4, 0)
         cv2.imshow('WebCam', dst)
         if cv2.waitKey(1) == ord('q'):
@@ -47,6 +47,7 @@ def display_camera():
     cap.release()
     cv2.destroyAllWindows()
 
+# This function works but generates errors if antenna is offline
 def display_camera_and_sound():
     sound = Process(target=play_sound)
     display = Process(target=display_camera)
