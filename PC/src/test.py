c72b834bffdc41af5c82dfc575b854356bc9b5cb
jteglund <jonas.teglund.6494@student.uu.se>
2023-06-29 13:08:49 +0200

    Added a test to display a heatmap over live cam feed

--- PC/src/test.py
+++ PC/src/test.py
@@ -7,12 +7,12 @@ cap = cv2.VideoCapture(0)
 
 # Read logo and resize
 logo = cv2.imread('/home/mario/Pictures/heatmap.png')
-size = 600
-logo = cv2.resize(logo, (600, 600))
+#size = 600
+logo = cv2.resize(logo, (1280, 720))
 
 # Create a mask of logo
-img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
+#mg2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+#ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
 
 print("IMPORTANT: YOU NEED PHOTO PERMISSION IF YOU ARE AT A SECURE/VITAL INSTALLATION!!!")
 if input("Do you have a valid photo permission? (Y/n)") == "Y":
@@ -22,13 +22,13 @@ if input("Do you have a valid photo permission? (Y/n)") == "Y":
         ret, frame = cap.read()
         # Region of Image (ROI), where we want to insert logo
         #roi = frame[-size-10:-10, -size-10:-10]
-
         # Set an index of where the mask is
         #roi[np.where(mask)] = 0
         #roi += logo
         #print(frame.shape)
         #print(logo.shape)
-        cv2.imshow('WebCam', frame)
+        dst = cv2.addWeighted(frame, 0.7, logo, 0.3, 0)
+        cv2.imshow('WebCam', dst)
         if cv2.waitKey(1) == ord('q'):
             break
 
