4d77df7061b17a9a401467103659322af6f8fb92
Irreq <irreq@protonmail.com>
2023-07-31 11:11:05 +0200

    Updated compiling

--- PC/benchmark/setup.py
+++ PC/benchmark/setup.py
@@ -10,9 +10,10 @@ import glob
 
 c_files = []
 c_files.extend(glob.glob("src/*.c"))
-# c_files.extend(glob.glob("src/antenna/*.c"))
+# print(c_files)
+c_files.extend(glob.glob("src/algorithms/*.c"))
 
-CFLAGS = "-O3 -march=native -mavx2 -lm -lrt -lasound -ljack -lpthread -lportaudio"
+CFLAGS = "-O3 -march=native -mavx2 -lm -lrt -lasound -ljack -lpthread -lportaudio -lm -Wall -O3 -march=native -mavx2 -lm -lrt -lasound -ljack -lpthread -lportaudio -lm"
 
 setup (
     name = 'Module',
@@ -30,6 +31,8 @@ setup (
             Extension("directions", ["src/directions.pyx"], include_dirs=["src/"], extra_compile_args = ["-lm"]),
             #Extension("visual", ["src/visual.pyx"], include_dirs=["src/"], extra_compile_args = ["-lm"]),
             Extension("kf", ["src/kf.pyx"], include_dirs=["src/"], extra_compile_args = ["-lm"]),
+            # Extension("mic", ["src/mic.pyx"]+c_files, include_dirs=["src/"], extra_compile_args = CFLAGS.split(" "), libraries=['portaudio']),
+            # Extension("mic", ["src/test/foo.pyx"]+glob.glob("src/test/*.c"), include_dirs=["src/test/"], extra_compile_args = CFLAGS.split(" "), libraries=['portaudio']),
         ],
 
         build_dir="build",
