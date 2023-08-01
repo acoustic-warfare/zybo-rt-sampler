5b4daf8484b7eea768e7eaeee99d49be8446fb08
jteglund <jonas.teglund.6494@student.uu.se>
2023-08-01 15:00:15 +0200

    Tweaked fft backend

--- PC/application/camera.py
+++ PC/application/camera.py
@@ -3,18 +3,18 @@ import cv2
 from multiprocessing import JoinableQueue, Process, Value
 
 from lib.beamformer import *
-from lib.visual import calculate_heatmap
+from lib.visual import calculate_heatmap, calculate_heatmap_fft
 
 import queue
 import config
 import numpy as np
-#from realtime_scripts.phase_shift_algorithm_peak_detection import main
+from realtime_scripts.phase_shift_algorithm_peak_detection import main
 WINDOW_DIMENSIONS = (1920, 1080)# (720, 480)
 APPLICATION_WINDOW_WIDTH, APPLICATION_WINDOW_HEIGHT = WINDOW_DIMENSIONS
 
 te = 0
 class VideoCamera(object):
-    def __init__(self):
+    def __init__(self, threshold):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
@@ -26,7 +26,8 @@ class VideoCamera(object):
         # as the main.py.
         # self.video = cv2.VideoCapture('video.mp4')
         self.v = Value('i', 1)
-
+        self.backend = 0
+        self.threshold = threshold
         self.setup()
     
     def __del__(self):
@@ -36,12 +37,13 @@ class VideoCamera(object):
         jobs = 1
         self.q = JoinableQueue(maxsize=2)
 
-        connect(True)
+        connect()
         self.processStarted = False
         self.p = Process(target=miso_api, args=(self.q, self.v))
-        self.p.start()
+        #self.p.start()
 
     def startBeamforming(self, backend = 0):
+        self.backend = backend
         if self.processStarted:
                 self.v.value = 0
                 self.p.join()
@@ -61,31 +63,36 @@ class VideoCamera(object):
         data = np.empty((config.N_MICROPHONES, config.N_SAMPLES), dtype=np.float32)
         while self.v.value:
             receive(data)
-            #heatmap_data = main(data.T)
-            #self.q.put(heatmap_data)
+            heatmap_data = main(data.T)
+            self.q.put(heatmap_data)
         
 
     def handle_image(self, image):
         image = cv2.flip(image, 1) # Nobody likes looking out of the array :(
         image = cv2.resize(image, WINDOW_DIMENSIONS)
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
+        # so we must encode it intonablePad/ JPEG in order to correctly display the
         # video stream.
         try:
             output = self.q.get(block=False)
             self.q.task_done()
-            res = calculate_heatmap(output)
-            res = cv2.resize(res, WINDOW_DIMENSIONS)
-            image = cv2.addWeighted(image, 0.6, res, 0.8, 0)
+            print(output.shape)
+            if self.backend != 2:
+                res, should_overlay = calculate_heatmap(output)
+            else:
+                res, should_overlay = calculate_heatmap_fft(output)
+            if should_overlay:
+                res = cv2.resize(res, WINDOW_DIMENSIONS)
+                image = cv2.addWeighted(image, 1.0, res, 0.8, 0)
 
         except queue.Empty:
-            pass
+            return image
 
         return image
     
     def get_frame(self):
         success, image = self.video.read()
-        #image = self.handle_image(image)
+        image = self.handle_image(image)
 
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
@@ -102,13 +109,12 @@ class VideoCamera(object):
         if self.processStarted:
             self.p.join()
             self.processStarted = False
-            
+
         disconnect()
         self.video.release()
 
 def gen(camera):
     while True:
-        print("HEJ")
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
